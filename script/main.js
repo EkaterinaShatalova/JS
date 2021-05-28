5be81c6f764f581be153f5456e7bbc7f867ac2d6
EkaterinaShatalova <c.shatalova@mail.ru>
2021-05-28 23:54:48 +0300

    feature lesson02-add

--- script/main.js
+++ script/main.js
@@ -1,9 +1,15 @@
 "use strict";
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission; 
-let period;
-alert('Привет!');
-console.log('Пока!');
+let num = 266219;
+let result = 1
+
+for (let index = 0; index < String(num).length; index++) {
+    result *= String(num)[index]   
+}
+console.log(result);
+
+let resultPow = result**3
+console.log(resultPow);
+
+console.log(String(resultPow).slice(0,2));
+
+
