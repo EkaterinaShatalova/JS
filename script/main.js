7dc8c38e5d70e6948965d8b003498e51aca33345
EkaterinaShatalova <c.shatalova@mail.ru>
2021-05-28 23:14:48 +0300

    feature lesson2

--- script/main.js
+++ script/main.js
@@ -1,11 +1,21 @@
 "use strict";
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission; 
-let period;
-alert('Привет!');
-console.log('Пока!');
-console.log('Пока!');
-console.log('Пока!');
+let money = 100000
+let income = 'ДиВиДенДы'
+let addExpenses = 'Интернет, Такси, Коммуналка';
+let deposit = true;
+let mission = 300000;
+let period = 3;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+console.log(addExpenses.toLowerCase().split(', '));
+let budgetDay = money/30;
+console.log(budgetDay);
+
+
+
+
