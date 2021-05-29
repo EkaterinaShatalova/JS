2e58fa1d81bbd5ea9baf34776d8b629291151c78
EkaterinaShatalova <c.shatalova@mail.ru>
2021-05-29 23:51:19 +0300

    fix lesson02 add

--- script/main.js
+++ script/main.js
@@ -1,13 +1,13 @@
 "use strict";
-let num = 266219;
-let result = 1
+const num = 266219;
+let result = 1;
 
 for (let index = 0; index < String(num).length; index++) {
-    result *= String(num)[index]   
+    result *= String(num)[index];  
 }
 console.log(result);
 
-let resultPow = result**3
+let resultPow = result**3;
 console.log(resultPow);
 
 console.log(String(resultPow).slice(0,2));
