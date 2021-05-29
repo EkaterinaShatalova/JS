66fb8e7d6919de4d7f4c00ae221c8a078ae25384
EkaterinaShatalova <c.shatalova@mail.ru>
2021-05-29 23:44:59 +0300

    fix lesson02

--- script/main.js
+++ script/main.js
@@ -1,10 +1,11 @@
 "use strict";
-let money = 100000
-let income = 'ДиВиДенДы'
-let addExpenses = 'Интернет, Такси, Коммуналка';
-let deposit = true;
-let mission = 300000;
-let period = 3;
+const money = 100000,
+income = 'ДиВиДенДы',
+addExpenses = 'Интернет, Такси, Коммуналка',
+deposit = true,
+mission = 300000,
+period = 3,
+budgetDay = money/30;
 
 console.log(typeof money);
 console.log(typeof income);
@@ -13,7 +14,7 @@ console.log(addExpenses.length);
 console.log(`Период равен ${period} месяцев`);
 console.log(`Цель заработать ${mission} рублей`);
 console.log(addExpenses.toLowerCase().split(', '));
-let budgetDay = money/30;
+
 console.log(budgetDay);
 
 
