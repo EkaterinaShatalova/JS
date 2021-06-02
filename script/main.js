ca150cd7fee3fa7d5dbad2a00453c48fe72299f3
EkaterinaShatalova <c.shatalova@mail.ru>
2021-06-03 01:05:39 +0300

    function check added

--- script/main.js
+++ script/main.js
@@ -1,67 +1,16 @@
 /*jshint esversion: 8 */
 /* jshint node: true */
 'use strict';
-const  income = 'ДиВиДенДы',
-      mission = 300000;
-const money = +prompt('Ваш месячный доход?', '10000');
-const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Квартплата, проездной, кредит');
-const deposit = !!prompt('Есть ли у вас депозит в банке?', 'Нет');
-const expenses1 = prompt('Введите обязательную статью расходов?', 'Квартплата');
-const amount1 = +prompt('Во сколько это обойдется?', '5000');
-const expenses2 = prompt('Введите обязательную статью расходов?', 'Проездной');
-const amount2 = +prompt('Во сколько это обойдется?', '1000');
 
-let accumulatedMonth = getAccumulatedMonth(money, amount1, amount2), 
-    budgetDay = Math.floor(accumulatedMonth / 30),
-    showTypeOf = function(data) {
-    console.log(typeof data);
-}
+function check(a) {
+    if  (typeof a !== 'string'  ) {
+        alert('Введены некорректные данные!');
+}  else if (a.length>30) {
+    console.log(a.slice(0,30)+'...');
+} else {
+    console.log(a.replace( /^\s+|\s+$/g, '' ));
+}}
 
-function getExpensesMonth(amount1, amount2) {
-    return amount1 + amount2;
-}
-
-function getAccumulatedMonth(money, amount1, amount2) {
-    let amount = getExpensesMonth(amount1, amount2);
-    return money - amount;
-}
-
-function getTargetMonth() {
-    return Math.ceil(mission / accumulatedMonth);
-}
-function getStatusIncome () {
-    switch(true) {
-        case (budgetDay >= 1200): 
-            console.log('У вас высокий уровень дохода');
-        break;
-    
-        case (budgetDay < 1200 && budgetDay >=600):
-            console.log('У вас средний уровень дохода');
-        break;
-    
-        case (budgetDay < 600 && budgetDay >=0):
-            console.log('К сожалению у вас уровень дохода ниже среднего');
-        break;
-    
-        default: 
-            console.log('Что то пошло не так');   
-    }
-}
-
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-console.log(`Расходы за месяц ${getExpensesMonth(amount1, amount2)}`);
-console.log(addExpenses.toLowerCase().split(', '));
-console.log(`Цель будет достигнута за ${getTargetMonth()} месяцев(-а)`);
-console.log(`Бюджет на день: ${budgetDay}`);
-getStatusIncome();
-
-
-
-
-
-
-
+check('    ffffffffff ааааааааааа    ');
 
 
