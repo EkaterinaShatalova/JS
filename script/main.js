78e19caee5db4a0dfca3b8a182363a65960d3420
EkaterinaShatalova <c.shatalova@mail.ru>
2021-06-03 22:03:09 +0300

    fix(script.js): change function 'check', add function 'isNumber'

--- script/main.js
+++ script/main.js
@@ -1,16 +1,22 @@
 /*jshint esversion: 8 */
 /* jshint node: true */
+
 'use strict';
 
-function check(a) {
-    if  (typeof a !== 'string'  ) {
+const isNumber = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+    };
+
+const check = function(a) {
+    let string = a;
+    if  (typeof string !== 'string' && isNumber(string)) {
         alert('Введены некорректные данные!');
-}  else if (a.length>30) {
-    console.log(a.replace( /^\s+|\s+$/g, '' ).slice(0,30)+'...');
-} else {
-    console.log(a.replace( /^\s+|\s+$/g, '' ));
+}  else if (string.length>30) {
+    console.log(string.trim().slice(0,30)+'...');
+} else {console.log(string.trim());
+
 }}
 
-check('    ffffffffff ааааааааааа  fffffffffffffffff   ');
+check('    ffffffffff ааааааааааа  ');
 
 
