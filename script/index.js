a9b59991382f64f8361e24e01ad220448df76b05
EkaterinaShatalova <c.shatalova@mail.ru>
2021-06-15 00:00:45 +0300

    refactor(index.js): unnecessary const removed

--- script/index.js
+++ script/index.js
@@ -2,15 +2,12 @@
 /* jshint node: true */
 'use strict';
 
-const incomeItems = document.querySelector('.income-items');
-const expensesItems = document.querySelector('.expenses-items');
-
 const salaryAmount = document.querySelector('.salary-amount');
-const incomeTitle = incomeItems.querySelector('.income-title');
+const incomeTitle = document.querySelector('input.income-title');
 const incomeAmount = document.querySelector('.income-amount');
 const btnPlusIncome = document.getElementsByTagName('button')[0];
 const addIncomeItem = document.querySelectorAll('.additional_income-item');
-const expensesTitle = expensesItems.querySelector('.expenses-title');
+const expensesTitle = document.querySelector('input.expenses-title');
 const expensesAmount = document.querySelector('.expenses-amount');
 const btnPlusExpenses = document.getElementsByTagName('button')[1];
 const addExpensesItem = document.querySelector('.additional_expenses-item');
