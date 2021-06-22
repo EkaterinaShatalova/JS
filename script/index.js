136b15629a43517c860240c199bcd54b6376cb91
EkaterinaShatalova <c.shatalova@mail.ru>
2021-06-22 23:18:30 +0300

    refactor(index.js): function added

--- script/index.js
+++ script/index.js
@@ -15,8 +15,7 @@ arrBooks[2].firstElementChild.firstElementChild.innerText = 'Книга 3. this 
 library.textContent = '';
 arrBooks.forEach(elem => {
     library.insertAdjacentElement('beforeend', elem);
-    }
-);
+});
 
 //задание 3
 document.body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
@@ -27,87 +26,49 @@ adv.remove();
 
 
 //задание 5
-//для книги 2
+const contentSorting = function(bookNumber) {
+    const bookElems = Array.from(arrBooks[bookNumber].firstElementChild.nextElementSibling.children);
 
-const book2elems = Array.from(arrBooks[1].firstElementChild.nextElementSibling.children);
+    let arrBook = [];
+    let arrChapter = [];
+    let arrApp = [];
 
-let arrBook2 = [];
-let arrChapter2 = [];
-let arrApp2 = [];
-
-book2elems.forEach(elem => {
-    if (elem.innerText === 'Предисловие') {
-        arrBook2[1] = elem;
-    }
-    else if (elem.innerText === 'Введение') {
-        arrBook2[0] = elem;
-    }
-    else if (elem.innerText.includes('Приложение')) {
-        if (elem.innerText.includes('Приложение A')) {
-            arrApp2[0] = elem;
+    bookElems.forEach(elem => {
+        if (elem.innerText === 'Предисловие') {
+            arrBook[1] = elem;
         }
-        if (elem.innerText.includes('Приложение B')) {
-            arrApp2[1] = elem;
+        else if (elem.innerText === 'Введение') {
+            arrBook[0] = elem;
         }
-        if (elem.innerText.includes('Приложение C')) {
-            arrApp2[2] = elem;
+        else if (elem.innerText.includes('Приложение')) {
+            if (elem.innerText.includes('Приложение A')) {
+                arrApp[0] = elem;
+            }
+            if (elem.innerText.includes('Приложение B')) {
+                arrApp[1] = elem;
+            }
+            if (elem.innerText.includes('Приложение C')) {
+                arrApp[2] = elem;
+            }
+            if (elem.innerText.includes('Приложение D')) {
+                arrApp[3] = elem;
+            }
         }
-        if (elem.innerText.includes('Приложение D')) {
-            arrApp2[3] = elem;
+        else {
+            arrChapter[Number(elem.innerText[6])-1] = elem;
         }
-    }
-    else {
-        arrChapter2[Number(elem.innerText[6])-1] = elem;
-    }
+    });
     
-});
-
-const arrBook2Final = arrBook2.concat(arrChapter2, arrApp2);
-
-arrBook2Final.forEach(elem => {
-    arrBooks[1].insertAdjacentElement('beforeend', elem);
-});
-
-// для 5й книги тож самое
-
-let arrBook5 = [];
-let arrChapter5 = [];
-let arrApp5 = [];
-
-const book5elems = Array.from(arrBooks[4].firstElementChild.nextElementSibling.children);
-
-book5elems.forEach(elem => {
-    if (elem.innerText === 'Предисловие') {
-        arrBook5[1] = elem;
-    }
-    else if (elem.innerText === 'Введение') {
-        arrBook5[0] = elem;
-    }
-    else if (elem.innerText.includes('Приложение')) {
-        if (elem.innerText.includes('Приложение A')) {
-            arrApp5[0] = elem;
-        }
-        if (elem.innerText.includes('Приложение B')) {
-            arrApp5[1] = elem;
-        }
-        if (elem.innerText.includes('Приложение C')) {
-            arrApp5[2] = elem;
-        }
-        if (elem.innerText.includes('Приложение D')) {
-            arrApp5[3] = elem;
-        }
-    }
-    else {
-        arrChapter5[Number(elem.innerText[6])-1] = elem;
-    }
+    const arrBookFinal = arrBook.concat(arrChapter, arrApp);
     
-});
+    arrBookFinal.forEach(elem => {
+        arrBooks[bookNumber].insertAdjacentElement('beforeend', elem);
+        }
+    );
+};
 
-const arrBook5Final = arrBook5.concat(arrChapter5, arrApp5);
-
-arrBook5Final.forEach(elem => {
-    arrBooks[4].insertAdjacentElement('beforeend', elem);
-});
+contentSorting(1);
+contentSorting(4);
 
 //задание 6
 const chapter8 = document.createElement('li');
