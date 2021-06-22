0ad01bb190cdb6615cfd21f661cc4a802051e915
EkaterinaShatalova <c.shatalova@mail.ru>
2021-06-22 22:34:00 +0300

    feature(index.js): order of books and chapters restored

--- script/index.js
+++ script/index.js
@@ -2,25 +2,130 @@
 /* jshint node: true */
 'use strict';
 
-const salaryAmount = document.querySelector('.salary-amount');
-const incomeTitle = document.querySelector('input.income-title');
-const incomeAmount = document.querySelector('.income-amount');
-const btnPlusIncome = document.getElementsByTagName('button')[0];
-const addIncomeItem = document.querySelectorAll('.additional_income-item');
-const expensesTitle = document.querySelector('input.expenses-title');
-const expensesAmount = document.querySelector('.expenses-amount');
-const btnPlusExpenses = document.getElementsByTagName('button')[1];
-const addExpensesItem = document.querySelector('.additional_expenses-item');
-const checkbox = document.querySelector('#deposit-check');
-const depositAmount = document.querySelector('.deposit-amount');
-const depositPercent = document.querySelector('.deposit-percent');
-const targetAmount = document.querySelector('.target-amount');
-const periodSelect = document.querySelector('.period-select');
-const budgetMonth = document.querySelector('.budget_month-value');
-const resBudgetDay = document.getElementsByClassName('result-total')[1];
-const resExpensesMonth = document.getElementsByClassName('result-total')[2];
-const resAddIncome = document.getElementsByClassName('result-total')[3];
-const resAddExpenses = document.getElementsByClassName('result-total')[4];
-const resIncomePeriod = document.getElementsByClassName('result-total')[5];
-const resTargetMonth = document.getElementsByClassName('result-total')[6];
-const btnStart = document.getElementById('start');
+//задание 1 и 2
+const book = document.querySelectorAll('.book');
+const library = document.querySelector('.books');
+let arrBooks = [];
+
+book.forEach(elem => {
+    arrBooks[Number(elem.firstElementChild.firstElementChild.innerText[6]-1)] = elem;
+});
+arrBooks[2].firstElementChild.firstElementChild.innerText = 'Книга 3. this и Прототипы Объектов';
+
+library.textContent = '';
+arrBooks.forEach(elem => {
+    library.insertAdjacentElement('beforeend', elem);
+    }
+);
+
+//задание 3
+document.body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
+
+//задание 4
+const adv = document.querySelector('.adv');
+adv.remove();
+
+
+//задание 5
+//для книги 2
+
+const book2elems = Array.from(arrBooks[1].firstElementChild.nextElementSibling.children);
+
+let arrBook2 = [];
+let arrChapter2 = [];
+let arrApp2 = [];
+
+book2elems.forEach(elem => {
+    if (elem.innerText === 'Предисловие') {
+        arrBook2[1] = elem;
+    }
+    else if (elem.innerText === 'Введение') {
+        arrBook2[0] = elem;
+    }
+    else if (elem.innerText.includes('Приложение')) {
+        if (elem.innerText.includes('Приложение A')) {
+            arrApp2[0] = elem;
+        }
+        if (elem.innerText.includes('Приложение B')) {
+            arrApp2[1] = elem;
+        }
+        if (elem.innerText.includes('Приложение C')) {
+            arrApp2[2] = elem;
+        }
+        if (elem.innerText.includes('Приложение D')) {
+            arrApp2[3] = elem;
+        }
+    }
+    else {
+        arrChapter2[Number(elem.innerText[6])-1] = elem;
+    }
+    
+});
+
+const arrBook2Final = arrBook2.concat(arrChapter2, arrApp2);
+
+arrBook2Final.forEach(elem => {
+    arrBooks[1].insertAdjacentElement('beforeend', elem);
+});
+
+// для 5й книги тож самое
+
+let arrBook5 = [];
+let arrChapter5 = [];
+let arrApp5 = [];
+
+const book5elems = Array.from(arrBooks[4].firstElementChild.nextElementSibling.children);
+
+book5elems.forEach(elem => {
+    if (elem.innerText === 'Предисловие') {
+        arrBook5[1] = elem;
+    }
+    else if (elem.innerText === 'Введение') {
+        arrBook5[0] = elem;
+    }
+    else if (elem.innerText.includes('Приложение')) {
+        if (elem.innerText.includes('Приложение A')) {
+            arrApp5[0] = elem;
+        }
+        if (elem.innerText.includes('Приложение B')) {
+            arrApp5[1] = elem;
+        }
+        if (elem.innerText.includes('Приложение C')) {
+            arrApp5[2] = elem;
+        }
+        if (elem.innerText.includes('Приложение D')) {
+            arrApp5[3] = elem;
+        }
+    }
+    else {
+        arrChapter5[Number(elem.innerText[6])-1] = elem;
+    }
+    
+});
+
+const arrBook5Final = arrBook5.concat(arrChapter5, arrApp5);
+
+arrBook5Final.forEach(elem => {
+    arrBooks[4].insertAdjacentElement('beforeend', elem);
+});
+
+//задание 6
+const chapter8 = document.createElement('li');
+chapter8.innerText = 'Глава 8: За пределами ES6';
+arrBooks[5].firstElementChild.nextElementSibling.children[8].insertAdjacentElement('beforeend', chapter8);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
