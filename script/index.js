583c64d8df94387251dafb4805c8b679dd75e1a6
EkaterinaShatalova <c.shatalova@mail.ru>
2021-07-23 11:12:52 +0300

    feature(index.js): calculator added

--- script/index.js
+++ script/index.js
@@ -249,7 +249,7 @@ window.addEventListener('DOMContentLoaded', () => {
         document.addEventListener('input', event => {
             const target = event.target;
             if (target.classList.contains('calc-item')) {
-                target.value = target.value.replace(/\D/, "");
+                target.value = target.value.replace(/\D\./, "");
             } else if (target.name === "user_name") {
                 target.value = target.value.replace(/[^А-Яа-я() -]/gi, "");
             } else if (target.name === "user_phone") {
@@ -271,6 +271,41 @@ window.addEventListener('DOMContentLoaded', () => {
         }, true);
     };
     check();
+
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block');
+        const calcType = document.querySelector('.calc-type');
+        const calcSquare = document.querySelector('.calc-square');
+        const calcCount = document.querySelector('.calc-count');
+        const calcDay = document.querySelector('.calc-day');
+        const calcTotal = document.querySelector('.calc-total');
+        const countSum = () => {
+            let total = 0;
+            let countValue = 1;
+            let dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value;
+            const squareValue = +calcSquare.value;
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+            calcTotal.textContent = total;
+        };
+        calcBlock.addEventListener('change', event => {
+            const target = event.target;
+            if (target.matches('select') || target.matches('input')) {
+                countSum();
+            }
+        });
+    };
+    calc(100);
 });
 
 
